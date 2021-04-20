78e62cca4ac3b12a5e5527c9948b58585c9b9066
log-box <a.y.logvin@gmail.com>
2021-04-20 08:43:22 +0300

    Done. Comment - best solutions

--- Mumbling.py
+++ Mumbling.py
@@ -6,4 +6,25 @@
 
 def accum(s):
     _string = ''
-    for _char in s:
+    for _char in range(len(s)):
+        _string += s[_char].upper() + s[_char].lower()*_char
+        if (_char + 1) < len(s):
+            _string += '-'
+    return _string
+
+# def accum(s):
+#     return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s))
+
+# >>> spisok = [16, 46, 26, 36]
+# >>> for i in enumerate(spisok):
+# ...     print(i)
+# ...
+# (0, 16)
+# (1, 46)
+# (2, 26)
+# (3, 36)
+
+
+if __name__ == '__main__':
+    print(accum("abcd"))
+
