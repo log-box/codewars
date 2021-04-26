93ff5620eee28042268dee420210305fdc3919f8
log-box <a.y.logvin@gmail.com>
2021-04-26 14:08:32 +0300

    start

---
+++ Snail.py
@@ -0,0 +1,46 @@
+# Snail Sort
+#
+# Given an n x n array, return the array elements arranged from outermost elements to the middle element,
+# traveling clockwise.
+#
+# array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+# snail(array) #=> [1,2,3,6,9,8,7,4,5]
+#
+# For better understanding, please follow the numbers of the next array consecutively:
+#
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+# snail(array) #=> [1,2,3,4,5,6,7,8,9]
+#
+# This image will illustrate things more clearly:
+#
+# NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array
+# in a clockwise snailshell pattern.
+#
+# NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+
+
+def snail(snail_map):
+    # snail_map = [[]]
+    for string in range(len(snail_map)):
+        for row in range(len(snail_map[string])):
+            print(snail_map[row][string], end=' ')
+
+
+snail([[1, 2, 3], [8, 9, 4], [7, 6, 5]])
+
+# array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+# expected = [1,2,3,6,9,8,7,4,5]
+# test.assert_equals(snail(array), expected)
+#
+#
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+# expected = [1,2,3,4,5,6,7,8,9]
+# test.assert_equals(snail(array), expected)
