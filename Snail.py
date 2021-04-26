fc68a571f75ac9e1eae8837e00720773501652c6
artem <a.y.logvin@gmail.com>
2021-04-26 20:31:51 +0300

    26.04.2021

--- Snail.py
+++ Snail.py
@@ -24,13 +24,36 @@
 
 
 def snail(snail_map):
-    # snail_map = [[]]
-    for string in range(len(snail_map)):
-        for row in range(len(snail_map[string])):
-            print(snail_map[row][string], end=' ')
+    snail_map = [[1, 2, 3, 5, 8],
+                 [8, 9, 4, 6, 9],
+                 [7, 6, 5, 7, 10],
+                 [1, 2, 3, 4, 11]]
+    temp_array_1 = []
+    while snail_map:
+        if not temp_array_1:
+            temp_array_1 = snail_map.pop(0)
+        # for i in range(len(snail_map)):
+        for j in range(len(snail_map)):
+            # for j in range(len(snail_map[i])-1):
+                # temp1 = snail_map[j]
+                # temp = snail_map[j].pop(len(temp1)-1)
+            temp_array_1.append(snail_map[j].pop(len(snail_map[j])-1))
+        test = range(len(snail_map))
+        for i in range(len(snail_map[len(snail_map)-1])):
+            # reverse_array = snail_map[len(snail_map)-1].reverse()
+            snail_map[len(snail_map) - 1].reverse()
+            temp_array_1.append(snail_map[i].pop(len(snail_map)-1))
+
+        print(temp_array_1)
+    print('done')
+    # for string in range(len(snail_map)):
+    #     for row in range(len(snail_map[string])):
+    #         print(snail_map[row][string], end=' ')
 
 
-snail([[1, 2, 3], [8, 9, 4], [7, 6, 5]])
+snail([[1, 2, 3],
+       [8, 9, 4],
+       [7, 6, 5]])
 
 # array = [[1,2,3],
 #          [4,5,6],
