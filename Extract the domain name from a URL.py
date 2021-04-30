9e3b1c00cb3a32d529b717d297c27ec1d6004a41
log-box <a.y.logvin@gmail.com>
2021-04-30 08:59:37 +0300

    done! best in comment

--- Extract the domain name from a URL.py
+++ Extract the domain name from a URL.py
@@ -13,13 +13,19 @@
 import re
 
 
+# def domain_name(url):
+#     result = re.findall(r'(http)?(https)?(://)?(www)?([a-zA-Z0-9-]+)\.', url)
+#     for i in result:
+#         for j in i:
+#             if j != '' and j != 'www' and j != 'http' and j != 'https' and j != '://':
+#                 return j
+#     return None
+
 def domain_name(url):
-    # result = re.findall(r'^[wWHhTtPpSs:/.]+([a-z0-9A-Z-]+)', url)
-    pattern = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
-    result = re.match(pattern, url)
-    return result[0]
+    return url.split("//")[-1].split("www.")[-1].split(".")[0]
 
 print(domain_name("http://github.com/carbonfive/raygun"))
+print(domain_name("www.xakep.ru"))
 print(domain_name("http://www.zombie-bites.com"))
 print(domain_name("https://www.cnet.com"))
 print(domain_name("http://google.com"))
