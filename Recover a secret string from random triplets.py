bd3b12f9ea611ac80df9868c6f9ad45bcf7548c8
log-box <a.y.logvin@gmail.com>
2021-05-17 10:43:22 +0300

    done! Not optimal. Best in comment

--- Recover a secret string from random triplets.py
+++ Recover a secret string from random triplets.py
@@ -22,32 +22,48 @@
 
 
 def recoverSecret(triplets):
-
     result_array = set()
     for i in range(len(triplets)):
         for j in range(len(triplets[i])):
             result_array.add(triplets[i][j])
     result_array = list(result_array)
-    for i in range(len(triplets)):
-        for j in range(len(triplets[i])):
-            temp = triplets[i].index(triplets[i][j])
+    stop = 0
+    while stop != 3:
+        for i in range(len(triplets)):
+            if result_array.index(triplets[i][0]) > result_array.index(triplets[i][1]):
+                result_array[result_array.index(triplets[i][0])], result_array[result_array.index(triplets[i][1])] = \
+                    result_array[result_array.index(triplets[i][1])], result_array[result_array.index(triplets[i][0])]
+            if result_array.index(triplets[i][1]) > result_array.index(triplets[i][2]):
+                result_array[result_array.index(triplets[i][1])], result_array[result_array.index(triplets[i][2])] = \
+                    result_array[result_array.index(triplets[i][2])], result_array[result_array.index(triplets[i][1])]
+        stop +=1
 
-
-
-
-    print(result_array)
+    return ''.join(result_array)
 
 
 # secret = "whatisup"
 triplets = [
-  ['t','u','p'],
-  ['w','h','i'],
-  ['t','s','u'],
-  ['a','t','s'],
-  ['h','a','p'],
-  ['t','i','s'],
-  ['w','h','s']
+    ['t', 'u', 'p'],
+    ['w', 'h', 'i'],
+    ['t', 's', 'u'],
+    ['a', 't', 's'],
+    ['h', 'a', 'p'],
+    ['t', 'i', 's'],
+    ['w', 'h', 's']
 ]
-recoverSecret(triplets)
+print(recoverSecret(triplets))
 
-#'w' 'h' 'a' 't' 'i' 's' 'u' 'p'
+# 'w' 'h' 'a' 't' 'i' 's' 'u' 'p'
+# def recoverSecret(triplets):
+#     r = list(set([i for l in triplets for i in l]))
+#     for l in triplets:
+#         fix(r, l[1], l[2])
+#         fix(r, l[0], l[1])
+#     return ''.join(r)
+#
+#
+# def fix(l, a, b):
+#     """let l.index(a) < l.index(b)"""
+#     if l.index(a) > l.index(b):
+#         l.remove(a)
+#         l.insert(l.index(b), a)
