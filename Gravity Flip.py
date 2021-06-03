344089b1a773eca6fa4bcbfaa50b2f7a1a2b6397
log-box <a.y.logvin@gmail.com>
2021-06-03 16:23:34 +0300

    done

--- Gravity Flip.py
+++ Gravity Flip.py
@@ -28,3 +28,22 @@
 
 
 def flip(d, a):
+    if d.lower() == 'r':
+        for i in range(len(a)-1):
+            for j in range(len(a)-i-1):
+                if a[j] > a[j+1]:
+                    a[j+1], a[j] = a[j], a[j+1]
+    if d.lower() == 'l':
+        for i in range(len(a) - 1):
+            for j in range(len(a) - i - 1):
+                if a[j] < a[j + 1]:
+                    a[j + 1], a[j] = a[j], a[j + 1]
+    print(a)
+
+
+flip('R', [3, 2, 1, 2])
+flip('L', [1, 4, 5, 3, 5])
+
+
+# def flip(d,a):
+#     return sorted(a, reverse=d=='L')
